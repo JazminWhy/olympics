83134b71bc61f83cd232372f19e6735540035527
Tido Marschall <timarsch@mail.uni-mannheim.de>
2018-10-29 11:34:20 +0100

    korrekter discipline comparer

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Comparators/AthleteParticipationMedal_inclYearDiscipline_Comparator.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Comparators/AthleteParticipationMedal_inclYearDiscipline_Comparator.java
@@ -2,8 +2,12 @@ package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparator
 
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.OlympicParticipation;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
+import au.com.bytecode.opencsv.CSVReader;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Athlete;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.ComparatorLogger;
@@ -17,6 +21,26 @@ public class AthleteParticipationMedal_inclYearDiscipline_Comparator implements 
 	private static final long serialVersionUID = 1L;
 
 	private ComparatorLogger comparisonLog;
+	
+	private boolean compareDiscipline(String disc1, String disc2) {
+		
+		try {
+			CSVReader reader = new CSVReader(new FileReader("20181025_discipline mapping_final.csv"));
+			List<String[]> DisciplineMapping = reader.readAll();
+			for (String[] string : DisciplineMapping) {
+				if (string[0].equals(disc1)) {
+					return string[1].equals(disc2);
+				}
+			}
+			return false;
+		} catch (Exception e) {
+			
+		} finally {
+			return false;
+		}
+	}
+	
+	
 
 	@Override
 	public double compare(Athlete record1, Athlete record2,
@@ -44,18 +68,19 @@ public class AthleteParticipationMedal_inclYearDiscipline_Comparator implements 
 			if (i.getYear() == 2016||i.getYear() == 1906) b_list.remove(i);
 		}
 		
+		
 		int total = Math.max(a_list.size(), b_list.size());
 		for (OlympicParticipation a : a_list) {
 			String medal_a = a.getMedal();
 			int year_a = a.getYear();
 			String discipline_a = a.getDisciplines();
-			String participation_a = medal_a + year_a + discipline_a;
+			String participation_a = medal_a + year_a;
 			for (OlympicParticipation b : b_list) {
 				String medal_b = b.getMedal();
 				int year_b = b.getYear();
 				String discipline_b = b.getDisciplines();
-				String participation_b = medal_b + year_b + discipline_b;
-				if (participation_a.equals(participation_b)) {
+				String participation_b = medal_b + year_b;
+				if (participation_a.equals(participation_b) && compareDiscipline(discipline_a, discipline_b)) {
 					same++;
 					b_list.remove(b);
 					break;
