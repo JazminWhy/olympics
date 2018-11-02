ecc27020c0a045988ed2984e121ddd6ba0fce56b
Marius Bock <marius.bock@protonmail.com>
2018-11-02 14:07:56 +0100

    Deleted movie files + Blocker Gym and Field + other stuff (Push it to the limit)

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_gymnast_fig_linear_combination.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_gymnast_fig_linear_combination.java
@@ -2,24 +2,13 @@ package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution;
 
 
 import java.io.File;
-import java.util.List;
 import org.apache.logging.log4j.Logger;
-
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyByBirthdayYearGenerator;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyByEarliestParticipationYearGenerator;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyByNameFirstLetters;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyByNationality;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyByGender;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.AthleteBlockingKeyGymnast;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.*;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Athlete;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.AthleteXMLReader;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.OlympicParticipation;
-import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.OlympicParticipationXMLReader;
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEvaluator;
-import de.uni_mannheim.informatik.dws.winter.matching.algorithms.MaximumBipartiteMatchingAlgorithm;
-import de.uni_mannheim.informatik.dws.winter.matching.blockers.NoBlocker;
-import de.uni_mannheim.informatik.dws.winter.matching.blockers.SortedNeighbourhoodBlocker;
 import de.uni_mannheim.informatik.dws.winter.matching.blockers.StandardRecordBlocker;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.LinearCombinationMatchingRule;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
@@ -27,7 +16,6 @@ import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
 import de.uni_mannheim.informatik.dws.winter.model.MatchingGoldStandard;
 import de.uni_mannheim.informatik.dws.winter.model.Performance;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
-import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record;
 import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
 import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
@@ -47,6 +35,7 @@ public class IR_gymnast_fig_linear_combination {
 	 *
 	 */
 
+	@SuppressWarnings("unused")
 	private static final Logger logger = WinterLogManager.activateLogger("default");
 	
     public static void main( String[] args ) throws Exception
@@ -59,51 +48,26 @@ public class IR_gymnast_fig_linear_combination {
 		HashedDataSet<Athlete, Attribute> dataAthletesFigshare = new HashedDataSet<>();
 		new AthleteXMLReader().loadFromXML(new File("data/input/20181029_figshare_Final.xml"), "/WinningAthletes/Athlete", dataAthletesFigshare);
 		
-		Athlete a = dataAthletesGymnast.getRecord("G-100001");
-		Athlete p = dataAthletesFigshare.getRecord("fig_10004");		
-		
-		//for(int i =0; i < op.size(); i++) {
-		//	OlympicParticipation op1 = (OlympicParticipation) op.get(0);
-		//	System.out.println(op1.getCity());
-		//}
-//		System.out.println(a.getName());
-//		System.out.println(a.getHeight());
-//		System.out.println(a.getWeight());
-//		System.out.println(a.getBirthday());
-//		System.out.println(a.getNationality());
-//		System.out.println(a.getSex());
-		
-		
-		
-		//System.out.println(dataAthletes.getRecord("G-100001"));
-		//System.out.println(dataOlympicParticipation.size());
-		//System.out.println(dataAthletes);
-		//System.out.println(dataOlympicParticipation.size());
-		
+
 		// load the training set
 		MatchingGoldStandard kfTraining = new MatchingGoldStandard();
-		kfTraining.loadFromCSVFile(new File("data/goldstandard/gs_fig_leicht_v1.csv"));
+		kfTraining.loadFromCSVFile(new File("data/goldstandard/gs_fig_gym_v1.csv"));
 
 		// create a matching rule
 		LinearCombinationMatchingRule<Athlete, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
-				0.01);
+				0);
 		matchingRule.activateDebugReport("data/output/debugResultsMatchingRule.csv", -1, kfTraining);
 		
 		// add comparators
-		//matchingRule.addComparator(new AthleteNameComparatorMongeElkan(), 0.25);
+		matchingRule.addComparator(new AthleteNameComparatorMongeElkan(), 1);
 		//matchingRule.addComparator(new AthleteParticipationMedal_inclYearDiscipline_Comparator(), 0.6);
-		matchingRule.addComparator(new AthleteNameComparatorLevenshtein(), 1);
+		//matchingRule.addComparator(new AthleteNameComparatorLevenshtein(), 1);
 		//matchingRule.addComparator(new AthleteSexComparator(), 0.5);
 		//matchingRule.addComparator(new AthleteNameComparatorLevenshtein(), 0.5);
-
-		//matchingRule.addComparator(new AthleteBirthdayComparator2Years(), 0.25);
 		
 		// create a blocker (blocking strategy)
-//		StandardRecordBlocker<Athlete, Attribute> blocker = new StandardRecordBlocker<Athlete, Attribute>(new AthleteBlockingKeyByNameFirstLetters());
-//		NoBlocker<Athlete, Attribute> blocker = new NoBlocker<>();
-//		SortedNeighbourhoodBlocker<Athlete, Attribute, Attribute> blocker = new SortedNeighbourhoodBlocker<>(new AthleteBlockingKeyByEarliestParticipationYearGenerator(), 1500);
-		StandardRecordBlocker<Athlete, Attribute> blocker = new StandardRecordBlocker<Athlete, Attribute>(new AthleteBlockingKeyByGender());
-		
+		StandardRecordBlocker<Athlete, Attribute> blocker = new StandardRecordBlocker<Athlete, Attribute>(new AthleteBlockingKeyGymnast());
+	
 		blocker.setMeasureBlockSizes(true);
 		//Write debug results to file:
 		blocker.collectBlockSizeData("data/output/debugResultsBlockingTest.csv", 100);
